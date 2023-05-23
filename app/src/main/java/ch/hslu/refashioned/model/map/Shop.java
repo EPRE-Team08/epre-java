499118508bec861f7d188ab2a5b62ff4287ecd3a
Luca Kyburz <kyburzluca@gmail.com>
2023-05-23 19:15:43 +0200

    Map: Read name and address on selection

--- app/src/main/java/ch/hslu/refashioned/model/map/Shop.java
+++ app/src/main/java/ch/hslu/refashioned/model/map/Shop.java
@@ -2,14 +2,22 @@ package ch.hslu.refashioned.model.map;
 
 import com.google.android.gms.maps.model.LatLng;
 
-public final class Shop {
+import ch.hslu.refashioned.ui.speech.Speakable;
+
+public final class Shop implements Speakable {
     private final Type type;
     private final String name;
+    private final String address;
+    private final String city;
+    private final String country;
     private final LatLng location;
 
-    public Shop(final Type type, final String name, final LatLng location) {
+    public Shop(final Type type, final String name, String address, String city, String country, final LatLng location) {
         this.type = type;
         this.name = name;
+        this.address = address;
+        this.city = city;
+        this.country = country;
         this.location = location;
     }
 
@@ -23,5 +31,10 @@ public final class Shop {
 
     public LatLng getLocation() {
         return location;
+    }
+
+    @Override
+    public String getSpeakableText() {
+        return this.name + " on " + this.address + " in " + this.city + ", " + this.country;
     }
 }
--- app/src/main/java/ch/hslu/refashioned/ui/map/MapFragment.java
+++ app/src/main/java/ch/hslu/refashioned/ui/map/MapFragment.java
@@ -1,25 +1,31 @@
 package ch.hslu.refashioned.ui.map;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import ch.hslu.refashioned.R;
+import java.util.Locale;
+import java.util.Optional;
 
-public class MapFragment extends Fragment {
+import ch.hslu.refashioned.R;
+import ch.hslu.refashioned.model.map.Shop;
+import ch.hslu.refashioned.ui.speech.Speaker;
+
+public final class MapFragment extends Fragment {
     private final MapViewModel viewModel;
+    private Speaker speaker;
 
     public MapFragment() {
         this.viewModel = new MapViewModel();
@@ -38,8 +44,53 @@ public class MapFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(viewModel::populateMap);
+
+        if (mapFragment != null)
+            mapFragment.getMapAsync(this::populateMap);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (getActivity() != null)
+            this.speaker = new Speaker(getActivity().getApplicationContext(), Locale.UK);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        this.speaker.dispose();
+    }
+
+    private void populateMap(final GoogleMap map) {
+        for (Shop shop : this.viewModel.get()) {
+            MarkerOptions options = new MarkerOptions();
+            options.position(shop.getLocation());
+            options.title(shop.getName());
+
+            Marker marker = map.addMarker(options);
+            this.viewModel.put(marker, shop);
         }
+
+        map.setOnMarkerClickListener(this::readShop);
+
+        Optional<Shop> shop = this.viewModel.get().stream().findFirst();
+        shop.ifPresent(s ->
+                map.moveCamera(CameraUpdateFactory.newCameraPosition(
+                        CameraPosition.builder()
+                                .target(s.getLocation())
+                                .zoom(12f)
+                                .build())));
+    }
+
+    private boolean readShop(final Marker marker) {
+        Shop shop = this.viewModel.get(marker);
+        this.speaker.speak(shop);
+
+        // The return value specifies whether the event was handled and
+        // thus the default behaviour should be skipped
+        return false;
     }
 }
