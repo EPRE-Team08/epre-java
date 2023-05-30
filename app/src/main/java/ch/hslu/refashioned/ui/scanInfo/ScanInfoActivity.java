58e9d2d4ca2fe465cf5f81d834f03096801e1314
Pascal Gansner <pascal.gansner@stud.hslu.ch>
2023-05-30 15:23:13 +0200

    progress

---
+++ app/src/main/java/ch/hslu/refashioned/ui/scanInfo/ScanInfoActivity.java
@@ -0,0 +1,16 @@
+package ch.hslu.refashioned.ui.scanInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.hslu.refashioned.R;
+
+public class ScanInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_info);
+    }
+}
